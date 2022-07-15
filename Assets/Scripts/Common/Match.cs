980f2b0765903d0af0da0394ab36536edd24bdfe
NANI <57311180+NANI-cant@users.noreply.github.com>
2022-07-15 20:21:37 +0300

    Rework to LineFindings (#7)

--- Assets/Scripts/Common/Match.cs
+++ Assets/Scripts/Common/Match.cs
@@ -22,6 +22,15 @@ namespace TableLogic {
             _positions.Add(firstFigure.Position);
         }
 
+        public Match(Match match) {
+            _matchedFigures = new List<Figure>();
+            _positions = new List<Vector2Int>();
+
+            _targetId = match.TargetId;
+            _matchedFigures.AddRange(match.Figures);
+            _positions.AddRange(match.Positions);
+        }
+
         public bool TryToAdd(Figure figure) {
             if (figure == null) return false;
             if (figure.Id != _targetId) return false;
@@ -32,11 +41,29 @@ namespace TableLogic {
             return true;
         }
 
+        public bool TryToMerge(Match match) {
+            foreach (var figure in match.Figures) {
+                if (_matchedFigures.Contains(figure)) {
+                    _matchedFigures.AddRange(match.Figures);
+                    _positions.AddRange(match.Positions);
+
+                    _matchedFigures = _matchedFigures.Distinct().ToList();
+                    _positions = _positions.Distinct().ToList();
+
+                    return true;
+                }
+            }
+            return false;
+        }
+
         public override string ToString() {
             string result = $"Match \n";
             result += $"count: {Count}\n";
             foreach (var position in _positions) {
                 result += $"\t {position}\n";
+            }
+            foreach (var figure in _matchedFigures) {
+                result += $"\t {figure}\n";
             }
             return result;
         }
